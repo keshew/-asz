bbc9810c8229208fc731b575ce57a1a15018f74d
keshew <artyom.korotckov2017@yandex.ru>
2025-12-04 15:11:17 +0500

    Initial Commit

---
+++ Сasz/_aszApp.swift
@@ -0,0 +1,17 @@
+//
+//  _aszApp.swift
+//  Сasz
+//
+//  Created by Артём Коротков on 04.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct _aszApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
