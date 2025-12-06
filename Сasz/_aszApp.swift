458ae1264887a990e2968272461e9fc19626d9b8
keshew <artyom.korotckov2017@yandex.ru>
2025-12-06 13:27:25 +0500

    add fix

--- Сasz/_aszApp.swift
+++ Сasz/_aszApp.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 @main
 struct _aszApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
     init() {
         let stats = UserDefaultsManager.shared
@@ -17,5 +18,13 @@ struct _aszApp: App {
         WindowGroup {
             TabBarView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    static var orientationLock = UIInterfaceOrientationMask.landscape
+
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return AppDelegate.orientationLock
     }
 }
